524b92426548c0365a57b5d223e2e00c0fd14495
Vladimir <86762075+TaepSi@users.noreply.github.com>
2026-06-01 14:43:56 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,17 +1,21 @@
-from supabase import create_client
-from config import SUPABASE_URL, SUPABASE_KEY
+import os
+import psycopg2
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+conn = psycopg2.connect(DATABASE_URL)
+conn.autocommit = True
+
 
 def get_bad_words():
-    res = supabase.table("bad_words").select("word").execute()
-    return [x["word"] for x in res.data]
+    with conn.cursor() as cur:
+        cur.execute("SELECT word FROM bad_words")
+        return [row[0] for row in cur.fetchall()]
 
 
 def add_report(chat_id, reporter_id, text, reason):
-    supabase.table("reports").insert({
-        "chat_id": chat_id,
-        "reporter_id": reporter_id,
-        "message_text": text,
-        "reason": reason
-    }).execute()
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO reports (chat_id, reporter_id, message_text, reason)
+            VALUES (%s, %s, %s, %s)
+        """, (chat_id, reporter_id, text, reason))
