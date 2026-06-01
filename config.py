cd87132e5d56893aee755421b0b3850a1ca06cdc
Vladimir <86762075+TaepSi@users.noreply.github.com>
2026-06-01 14:48:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 VK_TOKEN = os.getenv("VK_TOKEN")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
