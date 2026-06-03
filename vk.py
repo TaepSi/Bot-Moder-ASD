26860bf99961e1bc3339ab2674dcd7f4273e5158
Vladimir <86762075+TaepSi@users.noreply.github.com>
2026-06-03 11:23:26 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -1,15 +1,18 @@
 import vk_api
+import random
 from config import VK_TOKEN
 
 vk_session = vk_api.VkApi(token=VK_TOKEN)
 vk = vk_session.get_api()
 
+
 def send_message(peer_id, text):
     vk.messages.send(
         peer_id=peer_id,
         message=text,
-        random_id=0
+        random_id=random.randint(1, 2**31)
     )
+
 
 def delete_message(message_id):
     vk.messages.delete(
