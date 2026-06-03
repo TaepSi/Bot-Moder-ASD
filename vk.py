6074e782fedb814c670fcd4eb51997566647f511
Vladimir <86762075+TaepSi@users.noreply.github.com>
2026-06-03 11:43:16 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -7,18 +7,11 @@ vk = vk_session.get_api()
 
 
 def send_message(peer_id, text):
-    vk.messages.send(
-        peer_id=peer_id,
-        message=text,
-        random_id=random.randint(1, 2**31)
-    )
-
-
-def delete_message(message_id):
     try:
-        vk.messages.delete(
-            message_ids=message_id,
-            delete_for_all=1
+        vk.messages.send(
+            peer_id=peer_id,
+            message=text,
+            random_id=random.randint(1, 2**31)
         )
     except Exception as e:
-        print("DELETE ERROR:", repr(e), flush=True)
+        print("SEND ERROR:", repr(e), flush=True)
